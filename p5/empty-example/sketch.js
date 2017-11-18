d7699b0c3875c76bd8ed0e55a8ff6c69f0b5ed90
MattPeachey <Matt@TAC2F.WPA.Dal.Ca>
2017-11-18 15:37:15 -0400

    collision

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -2,7 +2,7 @@ var player,player2;
 function setup() {
   createCanvas(800,400);
   player = createSprite(50,50,50,50);
-  player2 = createSprite(50,450,50,50);
+  player2 = createSprite(50,300,50,50);
     
     
 }
@@ -10,7 +10,7 @@ function setup() {
 function draw() {
   background(25,255,255);  
   player.setSpeed(3,90);
-  //player.collide(player2);
+  player.collide(player2);
     
     
   drawSprites();
