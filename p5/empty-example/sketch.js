3178b355756677634fbeb904b5b4ffece00f6f1f
MattPeachey <Matt@TAC2F.WPA.Dal.Ca>
2017-11-18 14:17:53 -0400

    Changed

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,8 +1,8 @@
 function setup() {
    createCanvas(640,480);
    background(20,142,229)
-   playerOne = new person(10, 460, 20, 20);
-   playerTwo = new person(40, 440, 40, 20);
+   playerOne = new person(10, 450, 20, 20);
+   playerTwo = new person(40, 430, 40, 20);
   // put setup code here
 }
 
@@ -10,17 +10,16 @@ function draw() {
 
   playerOne.display();
   playerTwo.display();
+    playerOne.collide(playerTwo);
   background(600,600,600);
     
   line(30, 50, 85, 75);
     stroke(255);
     
+
+    
     
 }
 function person(xPos, yPos, lenght, height){
 	rect(xPos, yPos, height, lenght);
-   // if(keyIsPressed(LEFT_ARROW)){
-     //xpos ++;   
-    //}
-    //}
 } 
