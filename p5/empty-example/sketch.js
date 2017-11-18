d0a496cda28a032d228260d8608b43d3ea59b004
MattPeachey <Matt@TAC2F.WPA.Dal.Ca>
2017-11-18 15:32:45 -0400

    Works

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,25 +1,17 @@
+var player,player2;
 function setup() {
-   createCanvas(640,480);
-   background(20,142,229)
-   playerOne = new person(10, 450, 20, 20);
-   playerTwo = new person(40, 430, 40, 20);
-  // put setup code here
+  createCanvas(800,400);
+  player = createSprite(50,50,50,50);
+  player2 = createSprite(50,450,50,50);
+    
+    
 }
 
 function draw() {
-
-  playerOne.display();
-  playerTwo.display();
-    playerOne.collide(playerTwo);
-  background(600,600,600);
-    
-  line(30, 50, 85, 75);
-    stroke(255);
-    
-
+  background(255,255,255);  
+  player.setSpeed(3,90);
+  //player.collide(player2);
     
     
+  drawSprites();
 }
-function person(xPos, yPos, lenght, height){
-	rect(xPos, yPos, height, lenght);
-} 
