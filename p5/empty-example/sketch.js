ee46b07cef481300d58ee6b26a94f0c2d98eb76f
SpiderDawg <travislambe@gmail.com>
2017-11-18 17:32:11 -0400

    gravity and movement

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,36 +1,35 @@
-var player,player2,line,imgP1;
+var player,player2,line;
+var gravity = 9.81;
+
 
 function setup() {
   createCanvas(800,400);
-  player = createSprite(50,390,50,50);
+  player = createSprite(50,50,50,50);
   //player2 = createSprite(50,300,50,50);
-  line = createSprite(400,400,800,75);
-  //player2 = create sprite(0,400,200,75);
-    
-  img = loadImage("assets/player1.png");
-  //player.addImage(img);
+  line = createSprite(0,400,8000,75);
+  frameRate(30);
     
 }
 
 function draw() {
+
   background(25,255,255);
   
- 
-      
-
+  player.addSpeed(gravity, 90);
   
-  player.collide(line);
-  if(keyDown("DOWN_ARROW")){
-      player.setVelocity(0,5);
-  }else if(keyDown("UP_ARROW")){
-    player.setVelocity(0,-5);
+  if(player.collide(line)){
+    player.velocity.y = 0;
+  }
+  if(keyWentDown(" ")){
+    player.addSpeed(-gravity-30, 90);
   }else if(keyDown("RIGHT_ARROW")){
-    player.setVelocity(5,0);
+    player.velocity.x+=1;
   }else if(keyDown("LEFT_ARROW")){
-    player.setVelocity(-5,0);
+    player.velocity.x-=1;
   }
   else{
-      player.setVelocity(0,0);
+     player.addSpeed(gravity, 90);
+     player.velocity.x=0;
   }
     
     
