e6d90e92f5e24a69638d06c40c88a6fef0ad3b5e
MattPeachey <Matt@TAC2F.WPA.Dal.Ca>
2017-11-18 13:45:06 -0400

    Changed Backgroud

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
    createCanvas(640,480);
-   background(128,98,34)
+   background(20,142,229)
    playerOne = new person(10, 460, 20, 20);
    playerTwo = new person(40, 440, 40, 20);
   // put setup code here
@@ -10,7 +10,12 @@ function draw() {
 
   playerOne.display();
   playerTwo.display();
-  background(600,600,600);  
+  background(600,600,600);
+    
+  line(30, 50, 85, 75);
+    stroke(255);
+    
+    
 }
 function person(xPos, yPos, lenght, height){
 	rect(xPos, yPos, height, lenght);
