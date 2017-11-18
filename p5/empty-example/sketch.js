8acda5d6e65ee0ccce13e5f6c6e9bb3306d3d601
SpiderDawg <travislambe@gmail.com>
2017-11-18 13:33:13 -0400

    made the players

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,11 +1,17 @@
 function setup() {
-    createCanvas(640,480);
-    background(128,98,34)
+   createCanvas(640,480);
+   background(128,98,34)
+   playerOne = new person(10, 460, 20, 20);
+   playerTwo = new person(40, 440, 40, 20);
+  // put setup code here
 }
 
 function draw() {
-    
-    
-    
-    ellipse(50, 50, 80, 80);
+
+  playerOne.display();
+  playerTwo.display();
+  background(600,600,600);  
+}
+function person(xPos, yPos, lenght, height){
+	rect(xPos, yPos, height, lenght);
 }
