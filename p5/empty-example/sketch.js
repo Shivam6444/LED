7cad5394fa1c50ccc8b0b13602b0cb5b9870135d
MattPeachey <Matt@TAC2F.WPA.Dal.Ca>
2017-11-18 16:18:57 -0400

    bitch

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,20 +1,21 @@
-var player,player2;
+var player,player2,line;
+
 function setup() {
   createCanvas(800,400);
   player = createSprite(50,50,50,50);
-  player2 = createSprite(50,300,50,50);
-    
+  //player2 = createSprite(50,300,50,50);
+  line = createSprite(0,400,8000,75);
     
 }
 
 function draw() {
   background(25,255,255);
-  line(0,325,800,325);
+  
  
       
 
-  player.collide(player2);
   
+  player.collide(line);
 
   if(keyDown("DOWN_ARROW")){
       player.setVelocity(0,5);
