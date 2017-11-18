7fca8f81b687af62c9560f4bdfb699182a95e0ef
MattPeachey <Matt@TAC2F.WPA.Dal.Ca>
2017-11-18 19:10:30 -0400

    YAAASSS BOYS

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,4 +1,5 @@
-var player,player2,line,box;
+var player,player2,line,box,coin;
+var score =10;
 var gravity = 1;
 var jumping;
 
@@ -9,25 +10,38 @@ function setup() {
   line = createSprite(0,400,8000,75);
   frameRate(30);
   player = createSprite(50,390,20,30);
+  player2 = createSprite(90,327,40,70);
   box = createSprite(450,250,100,40);
   box.shapeColor = color(19,197,154);
-  //box.setCollider(rectangle,0,0,50,40)
+  coin = createSprite(200,45,10,10);
+  coin.shapeColor = color(250,223,27);
+  
     
 }
 
 function draw() {
-
-  background(148,111,225);
+    
+    background(148,111,225);
+s = ("Score: " + score); 
+fill(255,255,255);
+text(s, 10, 10, 70, 80);
+textSize(350);
   
   player.addSpeed(gravity, 90);
   
-  if(player.collide(line)||player.collide(box)){
+  if(player.collide(line)||player.collide(box)||player.collide(player2)){
     player.velocity.y = 0;
     if(jumping) jumping = false;
     //player.velocity.x=0;
   }
+    
+    if(player2.collide(line)||player2.collide(box)||player2.collide(player)){
+    player2.velocity.y = 0;
+    if(jumping) jumping = false;
+    //player.velocity.x=0;
+  }
 
-  if(keyWentDown(" ") && !jumping){
+  if(keyWentDown("UP_ARROW") && !jumping){
 
     player.addSpeed(-gravity-17, 90);
     jumping = true;
@@ -41,6 +55,25 @@ function draw() {
      player.velocity.x=0;
   }
     
+    if(keyWentDown("w") && !jumping){
+
+    player2.addSpeed(-gravity-17, 90);
+    jumping = true;
+  }else if(keyDown("d")){
+    player2.velocity.x+=0.5;
+  }else if(keyDown("a")){
+    player2.velocity.x-=0.5;
+  }
+  else{
+     player2.addSpeed(gravity, 90);
+     player2.velocity.x=0;
+  }
+    
+    
+  if(player.collide(coin)){
+      coin.remove();
+      score++;
+  }
     
   drawSprites();
 }
